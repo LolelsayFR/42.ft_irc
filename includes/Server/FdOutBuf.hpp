a32ce0b3b0e139482a94b6f78d4e38907fed32b7
Arthur Girard <artgirar@c1r7p12.42lehavre.fr>
2025-09-30 16:20:45 +0200

    CHANGE | finished normed fdoutbuf

--- includes/Server/FdOutBuf.hpp
+++ includes/Server/FdOutBuf.hpp
@@ -6,7 +6,7 @@
 /*   By: artgirar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 15:33:44 by artgirar          #+#    #+#             */
-/*   Updated: 2025/09/30 16:08:52 by artgirar         ###   ########.fr       */
+/*   Updated: 2025/09/30 16:19:24 by artgirar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,18 @@
 
 class FdOutBuf : public std::streambuf {
 private:
-	int fd_;
+	int _fd;
 
 protected:
 	virtual int_type overflow(int_type c);
 	virtual std::streamsize xsputn(const char* s, std::streamsize num);
 
 public:
+	FdOutBuf(void);
 	FdOutBuf(int fd);
+	FdOutBuf(const FdOutBuf & copy);
+	FdOutBuf	&operator=(const FdOutBuf & other);
+	~FdOutBuf(void);
 };
 
 #endif
