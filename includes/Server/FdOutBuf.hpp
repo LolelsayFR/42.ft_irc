f162a48b8ac734ab8b88691f7b2b4bf5431fcd35
Arthur Girard <artgirar@c1r7p12.42lehavre.fr>
2025-09-30 16:11:20 +0200

    ADD | start cpp norming on fdoutbuf

--- includes/Server/FdOutBuf.hpp
+++ includes/Server/FdOutBuf.hpp
@@ -6,7 +6,7 @@
 /*   By: artgirar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 15:33:44 by artgirar          #+#    #+#             */
-/*   Updated: 2025/09/30 15:58:48 by artgirar         ###   ########.fr       */
+/*   Updated: 2025/09/30 16:08:52 by artgirar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,14 @@
 
 class FdOutBuf : public std::streambuf {
 private:
-    int fd_;
-    
+	int fd_;
+
 protected:
-    virtual int_type overflow(int_type c) {
-        if (c != -1) {
-            char z = c;
-            if (write(fd_, &z, 1) != 1) {
-                return -1;
-            }
-        }
-        return c;
-    }
-    
-    virtual std::streamsize xsputn(const char* s, std::streamsize num) {
-        return write(fd_, s, num);
-    }
-    
+	virtual int_type overflow(int_type c);
+	virtual std::streamsize xsputn(const char* s, std::streamsize num);
+
 public:
-    FdOutBuf(int fd) : fd_(fd) {}
+	FdOutBuf(int fd);
 };
 
 #endif
