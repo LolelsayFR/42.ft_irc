4fcdc9c698b4ed8654f68c4be1b7dd5ab21276b7
Emilien Maillet <emaillet@c1r7p11.42lehavre.fr>
2025-09-30 11:14:45 +0200

    REFACTOR | Basic main test for parsing exceptions

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,32 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 09:52:28 by emaillet          #+#    #+#             */
-/*   Updated: 2025/09/30 09:52:30 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/09/30 11:06:53 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <cstring>
+#include <iostream>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <cstdlib>
+
+#include "Lang.hpp"
+#include "Exception.hpp"
+
+int main(int argc, char const *argv[])
+{
+	try {		
+		int port;
+		(void) argc;
+		port = std::atoi(argv[1]);
+		if (port == 0)
+			throw (PortErrorException());
+		throw(PasswordErrorException());
+	}
+	catch (ParsingException& e){
+		std::cout << e.what() << std::endl;
+	}
+	return (0);
+}
