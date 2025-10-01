1c87bd2a44289c03f7c5c6f1856dfb3284cd0b6e
Arthur <68420548+Anakoni@users.noreply.github.com>
2025-10-01 17:02:20 +0200

    FIX | Sources.mk is back

---
+++ Sources.mk
@@ -0,0 +1,15 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Sources.mk                                         :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2025/03/10 15:57:52 by arthur            #+#    #+#              #
+#    Updated: 2025/09/30 11:49:23 by emaillet         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+SRC_MAIN := ./src/main.cpp ./src/Client/Client.cpp ./src/Errors/Exception.cpp ./src/Server/FdOutBuf.cpp ./src/Server/checkPass.cpp ./src/Server/Server.cpp 
+
+SRC_FILES := $(SRC_MAIN)
