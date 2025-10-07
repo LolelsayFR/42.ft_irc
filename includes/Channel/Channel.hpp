3702aafd4e906abeffc44b081b8b6e50aad8d3bb
emaillet <maillet.emilien@gmail.com>
2025-10-07 16:05:49 +0200

    ADD | Mode ok ?, add invite requierement

--- includes/Channel/Channel.hpp
+++ includes/Channel/Channel.hpp
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 17:46:54 by emaillet          #+#    #+#             */
-/*   Updated: 2025/10/07 15:00:12 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/10/07 15:41:13 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,8 @@ private:
 	std::string				_password;
 	bool					_needInvite;
 	bool					_needPassword;
-	bool					_maxClient;
+	bool					_needOpTopic;
+	int						_maxClient;
 	std::vector<Client*>	_joinedList;
 	std::vector<Client*>	_opList;
 	std::vector<Client*>	_inviteList;
